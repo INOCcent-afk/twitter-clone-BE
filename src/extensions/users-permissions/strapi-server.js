9342eb584bd84a372b8843c13d459b61d761cc85
daveinoc <michael828inoc@gmail.com>
2022-05-24 21:36:24 +0800

    add more entity

--- src/extensions/users-permissions/strapi-server.js
+++ src/extensions/users-permissions/strapi-server.js
@@ -16,7 +16,7 @@ module.exports = (plugin) => {
     const user = await strapi.entityService.findOne(
       "plugin::users-permissions.user",
       ctx.state.user.id,
-      { populate: ["image"] }
+      { populate: ["image", "following", "followers"] }
     );
 
     ctx.body = sanitizeOutput(user);
@@ -26,7 +26,7 @@ module.exports = (plugin) => {
     const user = await strapi.entityService.findOne(
       "plugin::users-permissions.user",
       ctx.params.id,
-      { populate: ["image"] }
+      { populate: ["image", "following", "followers"] }
     );
 
     ctx.body = sanitizeOutput(user);
@@ -35,7 +35,7 @@ module.exports = (plugin) => {
   plugin.controllers.user.find = async (ctx) => {
     const users = await strapi.entityService.findMany(
       "plugin::users-permissions.user",
-      { ...ctx.params, populate: ["image"] }
+      { ...ctx.params, populate: ["image", "following", "followers"] }
     );
 
     ctx.body = users.map((user) => sanitizeOutput(user));
